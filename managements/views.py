cf05c86156f344e37b5aa42a31ab7fbffc3d0439
beshakimi <w.user2022@gmail.com>
2023-10-23 17:54:28 +0430

    fixed update food ithem image

--- managements/views.py
+++ managements/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render, redirect
 from addProducts.models import foodModel
+from addProducts.models import DringModel
 
 # dashboar_view
 def dashboar_view(request):
@@ -34,12 +35,14 @@ def update_food_model_view(request, food_id):
         Title = request.POST['title']
         Price = request.POST['price']
         Description = request.POST['description']
-        Image = request.FILES['image']
         
-        food.Title = Title
+        if 'image' in request.FILES:
+            Image = request.FILES['image']
+            food.Image = Image
+        food.Title =Title
         food.Price = Price
         food.Description = Description
-        food.Image = Image
+        
         food.save()
         
         return redirect('/manage/dashboard')
@@ -54,5 +57,55 @@ def delete_food(request, food_id):
     if request.method == 'POST':
         food.delete()
         return redirect('create-food')
+   
+
+    # add drink view 
+def create_drink_model_view(request):
+    drinks = DringModel.objects.all().order_by('-id')
+    if request.method == 'POST':
+ 
+        Title = request.POST['title']
+        Price = request.POST['price']
+        Description = request.POST['description']
+        Image = request.FILES['image']
+        food = DringModel(Title=Title, Price=Price, Description=Description, Image=Image)
+        food.save()
+        return redirect('/manage/addDrink')
+ 
+    context = {
+        'drink':drinks
+    }
+    return render(request, 'admin/addDrink.html', context)
+
+# update drink view 
+def update_drink_model_view(request, drink_id):
+    drink = get_object_or_404(DringModel, id=drink_id)
+    
+    if request.method == 'POST':
+        Title = request.POST['title']
+        Price = request.POST['price']
+        Description = request.POST['description']
+        
+        if 'image' in request.FILES:
+            Image = request.FILES['image']
+            drink.Image = Image
+        
+        drink.Title = Title
+        drink.Price = Price
+        drink.Description = Description
+        drink.save()
+        
+        return redirect('/manage/dashboard')
+    else:
+        return render(request, 'admin/updatedrink.html', {'drink': drink})
+    
+
+# delete drink view 
+def delete_drink(request, drink_id):
+    drink = DringModel.objects.get(id=drink_id)
+    
+    if request.method == 'POST':
+        drink.delete()
+        return redirect('create-drink')
     
     
