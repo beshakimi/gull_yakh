631f4bb7e17d75d7f93e52e1e36f5101f2697fc6
beshakimi <w.user2022@gmail.com>
2023-10-23 15:35:07 +0430

    admin can well ubdate food

--- managements/urls.py
+++ managements/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.index, name='index'),
+    path('dashboard/', views.dashboar_view, name='dashboard'),
     path('addfood', views.create_food_model_view, name='create-food'),
     path('update-food/<int:food_id>/', views.update_food_model_view, name='update-food'),
     path('delete-food/<int:food_id>/', views.delete_food, name='delete-food'),
