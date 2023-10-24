9b45dfc3fb3c2e6cc93da07e155326a4b4d5eabe
beshakimi <w.user2022@gmail.com>
2023-10-24 10:02:11 +0430

    create add post in admin panel

--- managements/urls.py
+++ managements/urls.py
@@ -2,12 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # food urls
     path('dashboard/', views.dashboar_view, name='dashboard'),
     path('addfood', views.create_food_model_view, name='create-food'),
     path('update-food/<int:food_id>/', views.update_food_model_view, name='update-food'),
     path('delete-food/<int:food_id>/', views.delete_food, name='delete-food'),
-    path('addDrink', views.create_drink_model_view, name='create-drink'),
+
+    # drink urls
+    path('addDrink', views.create_drink_view, name='create-drink'),
     path('update-drink/<int:drink_id>/', views.update_drink_model_view, name='update-drink'),
     path('delete-drink/<int:drink_id>/', views.delete_drink, name='delete-drink'),
+
+    # post urls
+    path('addPost', views.create_post_view, name='create-post'),
+    path('update-post/<int:post_id>/', views.update_post_view, name='update-post'),
+    path('delete-post/<int:post_id>/', views.delete_post, name='delete-post'),
   
 ]
