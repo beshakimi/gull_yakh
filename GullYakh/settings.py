33c839157ab0dbe49afee550beb0ac9af9cd9779
beshakimi <w.user2022@gmail.com>
2023-10-30 09:55:35 +0430

    fixed shop cart

--- GullYakh/settings.py
+++ GullYakh/settings.py
@@ -136,3 +136,21 @@ MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# تنظیمات SMTP
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'your_username'
+EMAIL_HOST_PASSWORD = 'your_password'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
+# تنظیمات ارسال ایمیل
+DEFAULT_FROM_EMAIL = 'info@example.com'
+SERVER_EMAIL = 'admin@example.com'
--- accounts/urls.py
+++ accounts/urls.py
@@ -8,10 +8,19 @@ urlpatterns = [
     path('logout/', views.logout_page, name='logout'),
     path('register', views.register, name='register'),
     path('profile_view/<int:id>/', views.profile_edit_view, name='profile_view'),
+
+
+
     path('forget_password', views.forget_password_view, name='forget_password'),
     path('confirm_message', views.confirm_message_view, name='confirm_message'),
-    path('new_password', views.new_password_view, name='new_password'),
+    # path('new_password', views.new_password_view, name='new_password'),
     path('confirm_change_password', views.confirm_change_password_view, name='confirm_change_password'),
     path('change_password', views.change_password_view, name='change_password'),
     
+
+    
+
+
+
+
 ]
